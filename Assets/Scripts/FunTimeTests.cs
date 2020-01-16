2c4bfc6527000dfb2d6c8cc70dc828949dadc9d1
Carsten At Sheridan <38149039+Squeebo07@users.noreply.github.com>
2020-01-16 13:29:31 -0500

    Modified FunTimeTests script
    Changed SayStuff string variable to say "A large cat" instead of "Wu"

--- Assets/Scripts/FunTimeTests.cs
+++ Assets/Scripts/FunTimeTests.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class FunTimeTests : MonoBehaviour
 {
 
-    public const string SayStuff = " Wu Ate: ";
+    public const string SayStuff = " A large cat Ate: ";
     public float CheeseBugerCount, MaxChzCount;
     // Start is called before the first frame update
     void Start()
