c6fcd8ab59794faa27b58f63480da5233da13298
BigWuTang <zhangwu@sheridancollege.ca>
2020-01-16 12:44:49 -0500

    Test Code Push for merge conflict creation etc

--- Assets/Scripts/FunTimeTests.cs
+++ Assets/Scripts/FunTimeTests.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class FunTimeTests : MonoBehaviour
 {
+
+    public const string SayStuff = " Carsten Ate: ";
+    public float CheeseBugerCount, MaxChzCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SayCheese(CheeseBugerCount));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SayCheese(float count)
+    {
+        while(count < MaxChzCount)
+        {
+            Debug.Log(SayStuff + count + " Cheeseburgers");
+
+            yield return new WaitForSeconds(0.5f);
+
+            Debug.Log(" Munch Munch " + " he says its yummy!");
+
+            count++;
+        }
+
+        Debug.Log(" Man he ate too much cheeseburgers");
+        yield return null; 
     }
 }
