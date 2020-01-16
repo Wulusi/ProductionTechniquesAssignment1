fb0233f2add38b4952653638e1e223eadae44212
BigWuTang <zhangwu@sheridancollege.ca>
2020-01-16 13:47:27 -0500

    Test changes to branch

--- Assets/Scripts/FunTimeTests.cs
+++ Assets/Scripts/FunTimeTests.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class FunTimeTests : MonoBehaviour
 {
 
-    public const string SayStuff = " A large cat Ate: ";
+    public const string SayStuff = " A large cat Ate who was very hungry and thirsty one two three four: ";
     public float CheeseBugerCount, MaxChzCount;
     // Start is called before the first frame update
     void Start()
