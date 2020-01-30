b34b2c257885dee03daf7374e85d5c348b8faefc
Carsten At Sheridan <38149039+Squeebo07@users.noreply.github.com>
2020-01-30 13:35:56 -0500

    Added nav mesh

--- Assets/ToyRoomTowers/Scripts/EnemyBehaviour.cs
+++ Assets/ToyRoomTowers/Scripts/EnemyBehaviour.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+
+    public NavMeshAgent navAgent;
+    public List<GameObject> WayPoints = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        GoToSpot();
+    }
+
+    void GoToSpot()
+    {
+        navAgent.SetDestination(WayPoints[0].transform.position);
+
     }
 
     // Update is called once per frame
