1e57286d2bd6a4d60245e8a804ee0fbf5f3c94c2
Marfoire <44646509+Marfoire@users.noreply.github.com>
2020-02-20 14:08:13 -0500

    Optimized async projectile kill function to cancel when not needed

--- Assets/ToyRoomTowers/Scripts/ProjectileBehaviour.cs
+++ Assets/ToyRoomTowers/Scripts/ProjectileBehaviour.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 
-public class ProjectileBehaviour : MonoBehaviour, PooledObjInterface
+public class ProjectileBehaviour : MonoBehaviour
 {
     /// <summary>
-    /// The layer of object this projectile is looking to hit
+    /// The layer of object this projectile is looking to hit, defaults to 30 which is the "Enemies" layer
     /// </summary>
     [SerializeField]
     [Range(0, 31)]
@@ -33,23 +34,45 @@ public class ProjectileBehaviour : MonoBehaviour, PooledObjInterface
     /// </summary>
     public bool isFirstSpawned;
 
+    /// <summary>
+    /// The attached rigidbody component
+    /// </summary>
     private Rigidbody rb;
+
+    /// <summary>
+    /// Task cancellation token for the projectile kill
+    /// </summary>
+    readonly CancellationTokenSource killCancel = new CancellationTokenSource();
+
+    /// <summary>
+    /// Variable that represents the projectile kill task, allows collision call to cancel the async expiration 
+    /// </summary>
+    Task t;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();       
+    }
+
+    private void OnEnable()
+    {
         KillProjectile(); //ignore this underline
     }
+
 
     /// <summary>
     /// Asynchronous function that is called on Awake, this method kills the projectile after its lifetime expires
     /// </summary>
     private async Task KillProjectile()
     {
-        await Task.Delay((int)(projectileLifetime * 1000));
-        //Destroy(gameObject);
+        //Delay by the lifetime, and give the cancellation token needed to stop the task if the projectile hit its enemy
+        t = Task.Delay((int)(projectileLifetime * 1000), killCancel.Token);
+
+        //Call the delay task to start the expiration countdown
+        await t;
+
         //Instead of destroy send projectile back into Object Pool
-        this.gameObject.SetActive(false);
+        this.gameObject.SetActive(false);    
     }
 
     /// <summary>
@@ -69,19 +92,15 @@ public class ProjectileBehaviour : MonoBehaviour, PooledObjInterface
     {
         if (other.gameObject.layer == enemyLayer)
         {
+            //Damage the enemy the projectile just hit
             other.GetComponent<EnemyBehaviour>().LoseHP(damageValue);
-            //Destroy(gameObject);
-            //Instead of destroy send projectile back into Object Pool
+
+            //Tell the task cancellation token to cancel the delay task in KillProjectile because we're going to kill the projectile early
+            killCancel.Cancel();
+
+            //Instead of destroy send projectile back into Object Pool                   
             this.gameObject.SetActive(false);
         }
 
-    }
-
-    public void OnPooledObjSpawn()
-    {
-        if (isFirstSpawned)
-        {
-            KillProjectile();
-        }
     }
 }
